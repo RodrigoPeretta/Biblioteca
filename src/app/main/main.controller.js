c0176dedd80daf772cf87799b05c497389eeae8d
unknown <rodrigoperetta@gmail.com>
2016-04-04 21:57:59 -0300

    Revert "Categorias"
    This reverts commit fe65563f6a4132da4a5f3172208389b11afd9f0d.

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -13,37 +13,21 @@
     $scope.searchText    = '';
 
     getBooks();
-    /*
+
     $scope.$watch('searchText',function(newVal){
       if(newVal != ''){
-        getBooks();
-        vm.awesomeThings = vm.awesomeThings.filter(function(value){
-          return value.Category.toLowerCase().indexOf(newVal.toLowerCase()) > -1 
-                 || value.Books.filter(function(book){
-                      return 
-                    })
-          //return value.title.toLowerCase().startsWith(newVal.toLowerCase());
+        vm.awesomeThings = response.data.Books.filter(function(value){
+          return value.title.toLowerCase().startsWith(newVal.toLowerCase());
         })
-
-        console.log(vm.awesomeThings);
       }
       else
       {
         getBooks();
       }
-    });*/
-    
+    });
+
     function getBooks() {
-      var group = _.groupBy(response.data.Books, "Category");
-
-      vm.categories = $.map(group, function(Books, Category) {
-                              return {Category};
-                          });
-
       vm.awesomeThings = response.data.Books;
-
-      console.log(vm.awesomeThings);
-
     }
   }
 })();
